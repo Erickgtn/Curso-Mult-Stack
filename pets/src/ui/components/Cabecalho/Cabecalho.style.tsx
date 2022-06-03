2e6134f5ed37daced400b447aa3f23b94d8c1c60
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-06-02 22:55:11 -0300

    atualizações do pacote

---
+++ pets/src/ui/components/Cabecalho/Cabecalho.style.tsx
@@ -0,0 +1,12 @@
+import{styled} from '@mui/material';
+
+export const CabecalhoContainer = styled('header')`
+    color:red;
+    display:flex;
+    justify-content:center;
+    border-bottom: 1px solid #f0f0f0;
+`;
+export const Logo = styled('img')`
+width:230px;
+`;
+<CabecalhoContainer/>
